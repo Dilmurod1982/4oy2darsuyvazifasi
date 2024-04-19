f631ee81dedea3dd7d76b45d18a202ed69d34733
cipher <dilsurbet@hmail.com>
2024-04-19 20:24:12 +0500

    uy vazifasi

---
+++ app.js
@@ -0,0 +1,19 @@
+// Uy vazifasi 18 video
+
+let ism = prompt('Ismingizni kiriting:');
+
+let birinchi = ism.toLowerCase();
+
+let ikkinchi = birinchi.length;
+
+let uchinchi = birinchi.slice(0,1)
+
+let tortinchi = uchinchi.toUpperCase()
+
+let beshinch = birinchi.replace(uchinchi, tortinchi )
+
+alert(`Salom, ${beshinch}!`)
+
+
+
+
